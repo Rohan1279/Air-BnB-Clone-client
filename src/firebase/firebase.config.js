263481235bd4a4b699a1ceba30a8309c19f1ced0
Rohan1279 <muniruddinrohan@gmail.com>
2022-12-02 14:50:52 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+// Config code goes here
+const firebaseConfig = {
+  apiKey: "AIzaSyCZKzevaT2qXLefimjtclpH3G0eGAs0Mp4",
+  authDomain: "air-bnb-clone-c4746.firebaseapp.com",
+  projectId: "air-bnb-clone-c4746",
+  storageBucket: "air-bnb-clone-c4746.appspot.com",
+  messagingSenderId: "730358312817",
+  appId: "1:730358312817:web:9eb22c0855b76ebda2d0d0",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
